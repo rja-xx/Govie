bc1716fada4e779ff289059ea1f05bf9a7f4a25a
rogerjakobsen <roger.jakobsen@embriq.no>
2015-09-28 22:45:55 +0200

    Added basic user persist and fetch, and shell

---
+++ server/server.js
@@ -0,0 +1,83 @@
+// server.js
+
+// BASE SETUP
+// =============================================================================
+
+// call the packages we need
+var express    = require('express');        // call express
+var app        = express();                 // define our app using express
+var bodyParser = require('body-parser');
+var User     = require('./models/user');
+var mongoose   = require('mongoose');
+mongoose.connect('mongodb://localhost:27017/govie');
+// configure app to use bodyParser()
+// this will let us get the data from a POST
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+var port = process.env.PORT || 8080;        // set our port
+
+// ROUTES FOR OUR API
+// =============================================================================
+var router = express.Router();              // get an instance of the express Router
+
+router.use(function(req, res, next) {
+	// do logging
+	console.log('Something is happening.');
+	next(); // make sure we go to the next routes and don't stop here
+    });
+
+router.route('/users')
+
+// create a bear (accessed at POST http://localhost:8080/api/bears)
+    .post(function(req, res) {
+        
+	    var user = new User();      // create a new instance of the User model
+	    user.name = req.body.name;  // set the userss name (comes from the request)
+
+	    // save the bear and check for errors
+	    user.save(function(err) {
+		    if (err)
+			res.send(err);
+
+		    res.json({ message: 'User created!' });
+	    })
+    })
+        
+	
+
+    .get(function(req, res) {
+        User.find(function(err, users) {
+		if (err)
+		    res.send(err);
+
+		res.json(users);
+	});
+	});
+
+router.route('/users/:user_id')
+
+// get the bear with that id (accessed at GET http://localhost:8080/api/bears/:bear_id)
+    .get(function(req, res) {
+	    User.findById(req.params.user_id, function(err, user) {
+		    if (err)
+			res.send(err);
+		    res.json(user);
+		});
+	});	
+
+// test route to make sure everything is working (accessed at GET http://localhost:8080/api)
+router.get('/', function(req, res) {
+    res.json({ message: 'hooray! welcome to our api!' });   
+});
+
+// more routes for our API will happen here
+
+// REGISTER OUR ROUTES -------------------------------
+// all of our routes will be prefixed with /api
+app.use('/api', router);
+
+// START THE SERVER
+// =============================================================================
+app.listen(port);
+console.log('Magic happens on port ' + port);
