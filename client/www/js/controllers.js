015be13244c9bcc960dccb637d03f2981ad5d5ea
rogerjakobsen <roger.jakobsen@gmail.com>
2015-10-11 11:06:40 +0200

    Established token based auth scheme

--- client/www/js/controllers.js
+++ client/www/js/controllers.js
@@ -1,28 +1,116 @@
 angular.module('starter.controllers', [])
 
-.controller('DashCtrl', function($scope) {})
+    .controller('WallCtrl', function ($scope, $http, $localStorage, $ionicModal) {
+        $scope.$on('$ionicView.enter', function (e) {
+            var token = $localStorage.get("govie-auth-token");
+            $http.defaults.headers.common['x-access-token'] = token;
+            if (token) {
+                $http.get('http://localhost:8080/govie/wall').then(
+                    function (res) {
+                        $scope.username = res.data.username;
+                        $scope.message = res.data.message;
+                    },
+                    function () {
+                        $ionicModal.fromTemplateUrl('templates/login.html').then(function (modal) {
+                            $scope.modal = modal;
+                            modal.show();
+                        });
+                    });
+            } else {
+                $ionicModal.fromTemplateUrl('templates/choose.html').then(function (modal) {
+                    $scope.modal = modal;
+                    modal.show();
+                });
+            }
+        });
+    })
 
-.controller('ChatsCtrl', function($scope, Chats) {
-  // With the new view caching in Ionic, Controllers are only called
-  // when they are recreated or on app start, instead of every page change.
-  // To listen for when this page is active (for example, to refresh data),
-  // listen for the $ionicView.enter event:
-  //
-  //$scope.$on('$ionicView.enter', function(e) {
-  //});
+    .controller('ChooseCtrl', function ($scope, $ionicModal) {
+        $scope.openLogin = function () {
+            $ionicModal.fromTemplateUrl('templates/login.html').then(function (modal) {
+                $scope.modal.hide();
+                $scope.modal = modal;
+                modal.show();
+            });
+        };
+        $scope.openCreateUser = function () {
+            $ionicModal.fromTemplateUrl('templates/createUser.html').then(function (modal) {
+                $scope.modal.hide();
+                $scope.modal = modal;
+                modal.show();
+            });
+        };
+    })
 
-  $scope.chats = Chats.all();
-  $scope.remove = function(chat) {
-    Chats.remove(chat);
-  };
-})
+    .controller('ChatsCtrl', function ($localStorage) {
+        $localStorage.set("govie-auth-token", '');
+    })
 
-.controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
-  $scope.chat = Chats.get($stateParams.chatId);
-})
+    .controller('TabCtrl', function ($ionicModal, $scope, $localStorage) {
+        $scope.modal = null;
+        $ionicModal.fromTemplateUrl('templates/splash.html', {}).then(function (modal) {
+                modal.show();
+                var token = $localStorage.get("govie-auth-token");
+                if (token == null) {
+                    modal.hide();
+                    $ionicModal.fromTemplateUrl('templates/choose.html', {}).then(function (modal) {
+                        $scope.modal = modal;
+                        modal.show();
+                    });
+                }
+                else {
+                    modal.hide();
+                }
+            }
+        );
+    })
 
-.controller('AccountCtrl', function($scope) {
-  $scope.settings = {
-    enableFriends: true
-  };
-});
+    .controller('ChatDetailCtrl', function ($scope, $stateParams, Chats) {
+        $scope.chat = Chats.get($stateParams.chatId);
+    })
+
+    .controller('SplashCtrl', function ($scope, $stateParams) {
+        console.log('got splash');
+    })
+
+    .controller('CreateUserCtrl', function ($state, $scope, $http, $state, $localStorage) {
+
+
+        $scope.createUser = function (alias, username, password) {
+            var request = {
+                alias: alias,
+                username: username,
+                password: password
+            };
+            console.log(request);
+            $http.post('http://localhost:8080/addUser', request).then(function (res) {
+                    $localStorage.set("govie-auth-token", res.data.token);
+                    $http.defaults.headers.common['x-access-token'] = res.data.token;
+                    $scope.modal.hide();
+                    $state.go('tab.wall', {}, {reload: true});
+                },
+                function (err) {
+                    console.log(JSON.stringify(err))
+                });
+        }
+    })
+
+    .controller('LoginCtrl', function ($state, $scope, $http, $localStorage) {
+        $scope.login = function (username, password) {
+            var request = {username: username, password: password};
+            $http.post('http://localhost:8080/authenticate', request).then(function (res) {
+                    $localStorage.set("govie-auth-token", res.data.token);
+                    $http.defaults.headers.common['x-access-token'] = res.data.token;
+                    $scope.modal.hide();
+                    $state.go('tab.wall', {}, {reload: true});
+                },
+                function (err) {
+                    console.log(JSON.stringify(err))
+                });
+        };
+    })
+    .controller('AccountCtrl', function ($scope) {
+        $scope.settings = {
+            enableFriends: true
+        };
+    });
--- server/server.js
+++ server/server.js
@@ -4,20 +4,30 @@
 // =============================================================================
 
 // call the packages we need
-var express    = require('express');        // call express
-var app        = express();                 // define our app using express
+var express = require('express');        // call express
+var app = express();                 // define our app using express
 var bodyParser = require('body-parser');
-var User     = require('./model/user');
-var Config   = require('./config')
-var uuid         = require('uuid');
-var mongoose   = require('mongoose');
+var User = require('./model/user');
+var Config = require('./config')
+var uuid = require('uuid');
+var mongoose = require('mongoose');
 var crypto = require('crypto');
 var jwt = require('jsonwebtoken');
+var cors = require('cors');
 mongoose.connect(Config.database);
 // configure app to use bodyParser()
 // this will let us get the data from a POST
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
+app.use(cors());
+app.use(function (req, res, next) {
+    console.log("Adding access headers!");
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Methods", "POST, GET, PUT, DELETE, OPTIONS");
+    res.header("Access-Control-Allow-Credentials", "true");
+    next();
+});
 app.set('superSecret', Config.secret);
 
 var port = process.env.PORT || 8080;        // set our port
@@ -25,102 +35,98 @@ var port = process.env.PORT || 8080;        // set our port
 // ROUTES FOR OUR API
 // =============================================================================
 
-app.post('/addUser', function(req, res){
-		var user = new User();
-		user.alias = req.body.alias;
-		user.username = req.body.username;
-		user.salt = uuid.v4();
-		user.password = req.body.password;
-		user.password = crypto.createHmac('sha1', user.salt).update(user.password).digest('hex');
+app.post('/addUser', function (req, res) {
+    console.log("adding user: " + JSON.stringify(req.body));
 
-		user.save(function(err) {
-			if (err){
-				res.send(err);
-			}else{
-				var token = jwt.sign(user, app.get('superSecret'), {
-					 expiresInMinutes: 1
-				 });
-				res.status(200).json({ token: token });
-			}
-		})
+    var user = new User();
+    user.alias = req.body.alias;
+    user.username = req.body.username;
+    user.salt = uuid.v4();
+    user.password = req.body.password;
+    user.password = crypto.createHmac('sha1', user.salt).update(user.password).digest('hex');
+
+    user.save(function (err) {
+        if (err) {
+            res.send(err);
+            return res;
+        } else {
+            var token = jwt.sign(user, app.get('superSecret'), {
+                expiresInMinutes: 1
+            });
+            res.status(200).json({token: token});
+            return res;
+        }
+    })
 });
 
-app.post('/authenticate', function(req, res){
-	var password = req.body.password;
-		User.find({username: req.body.username}, function(err, user){
-			if(user[0].password === crypto.createHmac('sha1', user[0].salt).update(password).digest('hex')){
-				var token = jwt.sign(user, app.get('superSecret'), {
-					 expiresInMinutes: 1
-				 });
-				res.status(200).json({ token: token });
-
-			}else{
-				res.status(403).json({ message: "invalid login" });
-			}
-		});
-	});
+app.post('/authenticate', function (req, res) {
+    console.log("Authenticate user: "+ JSON.stringify(req.body))
+    var password = req.body.password;
+    User.find({username: req.body.username}, function (err, user) {
+        if (user[0].password === crypto.createHmac('sha1', user[0].salt).update(password).digest('hex')) {
+            var token = jwt.sign(user, app.get('superSecret'), {
+                expiresInMinutes: 1
+            });
+            res.status(200).json({token: token});
+            return res;
+        } else {
+            res.status(403).json({message: "invalid login"});
+            return res;
+        }
+    });
+});
 
 
 var router = express.Router();              // get an instance of the express Router
 
-router.use(function(req, res, next) {
-	// do logging
-	console.log('got request');
-	next(); // make sure we go to the next routes and don't stop here
+router.use(function (req, res, next) {
+
+    // check header or url parameters or post parameters for token
+    var token = req.body.token || req.query.token || req.headers['x-access-token'];
+    // decode token
+    if (token) {
+
+        // verifies secret and checks exp
+        jwt.verify(token, app.get('superSecret'), function (err, decoded) {
+            if (err) {
+                return res.json({success: false, message: 'Failed to authenticate token.'});
+            } else {
+                if(Array.isArray(decoded)) {
+                    req.decoded = decoded[0];
+                }else{
+                    req.decoded = decoded;
+                }
+                    next();
+            }
+        });
+
+    } else {
+
+        // if there is no token
+        // return an error
+        return res.status(403).send({
+            success: false,
+            message: 'No token provided.'
+        });
+
+    }
 });
 
-router.use(function(req, res, next) {
-
-  // check header or url parameters or post parameters for token
-  var token = req.body.token || req.query.token || req.headers['x-access-token'];
-
-  // decode token
-  if (token) {
-
-    // verifies secret and checks exp
-    jwt.verify(token, app.get('superSecret'), function(err, decoded) {
-      if (err) {
-        return res.json({ success: false, message: 'Failed to authenticate token.' });
-      } else {
-				console.log(decoded.username);
-					console.log(decoded.exp);
-        // if everything is good, save to request for use in other routes
-        req.decoded = decoded;
-        next();
-      }
-    });
-
-  } else {
-
-    // if there is no token
-    // return an error
-    return res.status(403).send({
-        success: false,
-        message: 'No token provided.'
-    });
-
-  }
-});
-
-router.route('/users').get(function(req, res) {
-	User.find(function(err, users) {
-		if (err)
-		res.send(err);
-
-		res.json(users);
-	});
+router.route('/wall').get(function (req, res) {
+    console.log("returning wall");
+    res.json({message: 'you got wall!', username: req.decoded.username});
 });
 
 // test route to make sure everything is working (accessed at GET http://localhost:8080/api)
-router.get('/', function(req, res) {
-	res.json({ message: 'hooray! welcome to our api!' });
+router.get('/', function (req, res) {
+    res.json({message: 'hooray! welcome to our api!'});
 });
 
 // more routes for our API will happen here
 
 // REGISTER OUR ROUTES -------------------------------
 // all of our routes will be prefixed with /api
-app.use('/api', router);
+app.use('/govie', router);
 
 // START THE SERVER
 // =============================================================================
