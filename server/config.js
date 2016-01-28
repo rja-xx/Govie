8c62901609b040125766f9c028d05a2b145c0d5e
rogerjakobsen <roger.jakobsen@embriq.no>
2016-01-28 20:45:43 +0100

    Added the movie database api key

--- server/config.js
+++ server/config.js
@@ -1,4 +1,5 @@
 module.exports = {
     'secret': 'fksadjhfkasdhfhiwuehfisr7237894eu2i3hwr7qtrfyguhsdjk',
-    'database': 'mongodb://localhost:27017/mongo'
+    'database': 'mongodb://localhost:27017/mongo',
+    'tmdb-api-key': 'f01976dbf3d2f2e2929368fe98ef4aea'
 };
