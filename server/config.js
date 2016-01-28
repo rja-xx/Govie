1ead4e82c0f59f7e54e271be8b68b13d10fbf474
rogerjakobsen <roger.jakobsen@embriq.no>
2016-01-28 22:51:10 +0100

    Added the google places api key

--- server/config.js
+++ server/config.js
@@ -1,5 +1,6 @@
 module.exports = {
     'secret': 'fksadjhfkasdhfhiwuehfisr7237894eu2i3hwr7qtrfyguhsdjk',
     'database': 'mongodb://localhost:27017/mongo',
-    'tmdb-api-key': 'f01976dbf3d2f2e2929368fe98ef4aea'
+    'tmdb-api-key': 'f01976dbf3d2f2e2929368fe98ef4aea',
+    'google-places-api-key': 'AIzaSyBri-BATjtjQwT96yJ9HAerE6Kqy1Vmbxo'
 };
