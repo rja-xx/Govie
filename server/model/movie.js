9fa7b08d6fb796aef15e4d2b77618848db944a71
roger <roger.jakobsen@gmail.com>
2016-09-19 09:07:51 +0200

    added case insensitive search

--- server/model/movie.js
+++ server/model/movie.js
@@ -3,6 +3,7 @@ var Schema       = mongoose.Schema;
 
 var MovieSchema  = new Schema({
     title: String,
+    title_lowercase: String,
     tmdbId: String,
     posterUrl: String,
     backdropUrl: String,
