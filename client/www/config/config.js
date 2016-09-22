99b6e07dc14f05d54da0717a1b68ef39e25e56c5
rogerjakobsen <roger.jakobsen@embriq.no>
2016-09-22 09:07:12 +0200

    docker settings

--- client/www/config/config.js
+++ client/www/config/config.js
@@ -1,8 +1,8 @@
 govieconfig = {
-  //url: 'http://2.248.64.177:28976',
-  //wsurl: 'ws://2.248.64.177:28976',
-  url: 'http://localhost:8773',
-  wsurl: 'ws://localhost:8773',
+  url: 'http://2.248.64.177:28976',
+  wsurl: 'ws://2.248.64.177:28976',
+  //url: 'http://localhost:8773',
+  //wsurl: 'ws://localhost:8773',
   twittersecret: 'POIFLLnvyTLZeXV5q3gawFpIMw1X1Wl3xfbz9oMMR0is4UAMJm',
   twitterkey: 'c70nU7fcLJk4Nu7KJFJZPSuIQ'
 };
--- server/config.js
+++ server/config.js
@@ -1,9 +1,9 @@
 module.exports = {
     'secret': 'fksadjhfkasdhfhids4234wuehfisr7237894eu2i3hwr7qtrfyguhsdjk',
-    'database': 'mongodb://localhost:27017/mongo',
-    //'database': 'mongodb://goviecache:27017/mongo',
-    'eventshost': 'localhost',
-    //'eventshost': 'govieevents',
+    //'database': 'mongodb://localhost:27017/mongo',
+    'database': 'mongodb://goviecache:27017/mongo',
+    //'eventshost': 'localhost',
+    'eventshost': 'govieevents',
 
     //http://image.tmdb.org/t/p/w300/z0WX3Oy73eJleWXRl87EsBg8z56.jpg
     'tmdb-api-key': 'f01976dbf3d2f2e2929368fe98ef4aea',
